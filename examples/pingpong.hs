0a4f52ed223641472b2416cc143387e3467982cf
shnarazk <narazakilaboratory@gmail.com>
2018-01-23 09:18:38 +0900

    works!

--- examples/pingpong.hs
+++ examples/pingpong.hs
@@ -3,15 +3,18 @@ import Data.Text
 import Pipes
 
 import Network.Discord
+import qualified Token as Secret
 
 reply :: Message -> Text -> Effect DiscordM ()
 reply Message{messageChannel=chan} cont = fetch' $ CreateMessage chan cont Nothing
 
 main :: IO ()
-main = runBot (Bot "TOKEN") $ do
-  with ReadyEvent $ \(Init v u _ _ _) ->
+main = runBot (Bot Secret.token) $ do
+  with ReadyEvent $ \(Init v u _ _ _) -> do
     liftIO . putStrLn $ "Connected to gateway v" ++ show v ++ " as user " ++ show u
+    fetch' (CreateMessage Secret.channelTest "Hello, World!" Nothing)
 
   with MessageCreateEvent $ \msg@Message{..} -> do
-    when ("Ping" `isPrefixOf` messageContent && (not . userIsBot $ messageAuthor)) $
+    when ("Ping" `isPrefixOf` messageContent && (not . userIsBot $ messageAuthor)) $ do
+      liftIO $ putStrLn "got a trigger"
       reply msg "Pong!"
